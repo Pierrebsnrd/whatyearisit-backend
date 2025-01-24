b127313518fa13abd71312f0ddbbc072673bee33
Pierre Boisnard <pierre.boisnard@live.fr>
2025-01-24 10:30:27 +0100

    update backend

---
+++ routes/year.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    const date = new Date();
+    res.json({ year : date });
+});
+
+module.exports = router;
